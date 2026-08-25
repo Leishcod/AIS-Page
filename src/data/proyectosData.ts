e62bed796ea7d165a11f0a1b0f82f18c6bf996e0
JefersonYat22 <jefersonjsy48@gmail.com>
2026-08-24 19:18:45 -0500

    feat(eventos): agregando primer proyecto oficial a la data y colocándolo en el hero

--- src/data/proyectosData.ts
+++ src/data/proyectosData.ts
@@ -1,21 +1,31 @@
 import type { Project, FAQ } from '../types/proyectos';
 
 export const mainProject: Project = {
-  id: 'icai-2027',
-  title: 'I Internacional Congress on Artificial Intelligence',
-  status: 'Próximamente',
-  shortDescription: 'Integra diez ejes temáticos que convocan a actores de naturaleza muy distinta entre sí: gobierno peruano, empresas privadas locales, empresas tecnológicas internacionales, organismos multilaterales y academia de investigación de alto nivel.',
-  startDate: 'Agosto 2027. Lima, Perú.',
-  members: [], // Hero doesn't strictly need members
+  id: 'landing-page-de-ais',
+  title: 'Landing Page de nuestro Centro Cultural',
+  status: 'En desarrollo',
+  shortDescription: 'La dirección de Tecnología e Investigación se encarga de la creación y mantenimiento de la página web institucional.',
+  fullDescription: 'La dirección de Tecnología e Investigación se encarga de la creación y mantenimiento de la página web institucional. Esta Landing Page tiene por objetivo mostrar información acerca de AIS. El equipo de Tecnología e Investigación actualizará la información siempre que haya información nueva. Este proyecto de software inicialmente solo contendrá frontend, pero más adelante se espera contar con un CMS para automatizar muchas funciones.',
+  startDate: '5 de Agosto del 2026',
+  members: [
+      { name: 'Leonel Cruzado', role: 'Líder de Proyecto' },
+      { name: 'Jeanpier Robles', role: 'Sublíder de proyecto' },
+      { name: 'John Tucto', role: 'Miembro' },
+      { name: 'Joaquín Berrospi', role: 'Miembro' },
+      { name: 'Diego Pinedo', role: 'Miembro' },
+      { name: 'Alejandro Velarde', role: 'Miembro' },
+      { name: 'Jeferson Yataco', role: 'Miembro' }
+    ]
 };
 
 export const projectsList: Project[] = [
   {
-    id: 'feria-tecnologica',
-    title: 'Feria tecnológica',
+    id: 'landing-page-de-ais',
+    title: 'Landing Page de nuestro Centro Cultural',
     status: 'En desarrollo',
-    shortDescription: 'Stands permanentes de empresas, universidades y startups durante los 5 días, abiertos al público general en horarios específicos',
-    startDate: '5 de Agosto',
+    shortDescription: 'La dirección de Tecnología e Investigación se encarga de la creación y mantenimiento de la página web institucional.',
+    fullDescription: 'La dirección de Tecnología e Investigación se encarga de la creación y mantenimiento de la página web institucional. Esta Landing Page tiene por objetivo mostrar información acerca de AIS. El equipo de Tecnología e Investigación actualizará la información siempre que haya información nueva. Este proyecto de software inicialmente solo contendrá frontend, pero más adelante se espera contar con un CMS para automatizar muchas funciones.',
+    startDate: '5 de Agosto del 2026',
     members: [
       { name: 'Leonel Cruzado', role: 'Líder de Proyecto' },
       { name: 'Jeanpier Robles', role: 'Sublíder de proyecto' },
